705a5ccaf3ae4ce66ec41f94d399d9fa68edbf86
GUILDER <pplg39395@gmail.com>
2026-02-13 14:53:38 -0400

    Implementacion de GESTION DE TIENDAS

--- apps/tiendas/urls.py
+++ apps/tiendas/urls.py
@@ -6,7 +6,7 @@ app_name = 'tiendas'
 urlpatterns = [
     path('', views.listar_tiendas, name='listar'),
     path('crear/', views.crear_tienda, name='crear'),
-    path('editar/<int:pk>/', views.editar_tienda, name='editar'),
-    path('cambiar-estado/<int:pk>/', views.cambiar_estado_tienda, name='cambiar_estado'),
-    path('obtener/<int:pk>/', views.obtener_tienda, name='obtener'),
+    path('<int:pk>/obtener/', views.obtener_tienda, name='obtener'),
+    path('<int:pk>/editar/', views.editar_tienda, name='editar'),
+    path('<int:pk>/cambiar-estado/', views.cambiar_estado_tienda, name='cambiar_estado'),
 ]
