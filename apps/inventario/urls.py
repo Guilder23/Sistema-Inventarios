bb93e18b9fb12a32425e5e08d17802baef395211
GUILDER <pplg39395@gmail.com>
2026-02-26 12:27:31 -0400

    Implementacion del INVENTARIO DEL DEPOSITO, aparece en el sidebar de rol TIENDA

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # Inventario
     path('', views.ver_inventario, name='ver_inventario'),
+    path('deposito/', views.ver_inventario_deposito, name='ver_inventario_deposito'),
     path('ubicacion/<int:ubicacion_id>/', views.ver_inventario_ubicacion, name='ver_inventario_ubicacion'),
     path('producto/<int:producto_id>/asignar-precio/', views.asignar_precio, name='asignar_precio'),
     
