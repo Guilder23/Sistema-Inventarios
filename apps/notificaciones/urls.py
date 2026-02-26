45e5254aa09f77f141914161499eee45734546c1
Parche <93171758+ParcheEnGit@users.noreply.github.com>
2026-02-26 18:59:18 -0400

    Rutas para notificaciones sse

--- apps/notificaciones/urls.py
+++ apps/notificaciones/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Notificaciones
+    # Notificaciones existentes
     path('', views.listar_notificaciones, name='listar_notificaciones'),
     path('obtener/', views.obtener_notificaciones, name='obtener_notificaciones'),
     path('contador/', views.contador_notificaciones, name='contador_notificaciones'),
     path('marcar-leida/<int:id>/', views.marcar_leida, name='marcar_notificacion_leida'),
     path('marcar-todas-leidas/', views.marcar_todas_leidas, name='marcar_todas_leidas'),
+    
+    #Notificaciones en tiempo real
+    path('stream/', views.notificaciones_sse, name='notificaciones_sse'),
 ]
