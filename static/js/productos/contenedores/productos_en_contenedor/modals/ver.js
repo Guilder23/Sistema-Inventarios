6d0753f1e26507d25f0e238cfeede92d0666c0bb
GUILDER <pplg39395@gmail.com>
2026-03-08 11:17:53 -0400

    Se muestra la cantidad recibida en productosContenedor

--- static/js/productos/contenedores/productos_en_contenedor/modals/ver.js
+++ static/js/productos/contenedores/productos_en_contenedor/modals/ver.js
@@ -11,18 +11,24 @@
             const nombre = $(this).data('nombre');
             const codigo = $(this).data('codigo');
             const cantidad = $(this).data('cantidad');
+            const cantidadRecibida = $(this).data('cantidad-recibida');
             
-            cargarYMostrarProducto(pcId, nombre, codigo, cantidad);
+            cargarYMostrarProducto(pcId, nombre, codigo, cantidad, cantidadRecibida);
         });
     });
 
-    function cargarYMostrarProducto(pcId, nombre, codigo, cantidad) {
-        console.log('Ver.js: Mostrando datos del producto:', { pcId, nombre, codigo, cantidad });
+    function cargarYMostrarProducto(pcId, nombre, codigo, cantidad, cantidadRecibida) {
+        console.log('Ver.js: Mostrando datos del producto:', { pcId, nombre, codigo, cantidad, cantidadRecibida });
+        
+        // Calcular cantidad movida
+        const cantidadMovida = cantidadRecibida - cantidad;
         
         $('#verPCId').text(pcId);
         $('#verNombre').text(nombre || 'No especificado');
         $('#verCodigo').text(codigo || 'No especificado');
-        $('#verCantidad').html(`<span class="badge-modal">${cantidad} unidades</span>`);
+        $('#verCantidadRecibida').html(`<span class="badge-modal-recibida">${cantidadRecibida} unidades</span>`);
+        $('#verCantidad').html(`<span class="badge-modal-disponible">${cantidad} unidades</span>`);
+        $('#verCantidadMovida').html(`<span class="badge-modal-movida">${cantidadMovida} unidades</span>`);
 
         console.log('Ver.js: Abriendo modal...');
         $('#modalVerProductoContenedor').modal('show');
